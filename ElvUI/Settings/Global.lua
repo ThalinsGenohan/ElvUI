47325c4ba30f5b53b3aaf897e58e91f18ab0f5b1
BanditTech <thebbandit@gmail.com>
2022-03-31 02:56:43 -0700

    Fix range check fader on unitframes
    primitive fix by merging all the classes into druid

--- ElvUI/Settings/Global.lua
+++ ElvUI/Settings/Global.lua
@@ -43,149 +43,58 @@ G.unitframe = {
 		otherFilter = "CCDebuffs",
 	},
 	spellRangeCheck = {
-		PRIEST = {
-			enemySpells = {
-				[585] = true, -- Smite (30 yards)
-			},
-			longEnemySpells = {
-				[589] = true, -- Shadow Word: Pain (30 yards)
-			},
-			friendlySpells = {
-				[2050] = true, -- Lesser Heal (40 yards)
-			},
-			resSpells = {
-				[2006] = true, -- Resurrection (40 yards)
-			},
-			petSpells = {},
-		},
 		DRUID = {
 			enemySpells = {
+				[75] = true, -- Auto Shot (35 yards)
+				[2136] = true, -- Fire Blast (20 yards)
+				[116] = true, -- Frostbolt (30 yards)
+				[12826] = true, -- Polymorph (30 yards)
 				[33786] = true, -- Cyclone (20 yards)
-			},
-			longEnemySpells = {
-				[5176] = true, -- Wrath (30 yards)
-			},
-			friendlySpells = {
-				[5185] = true, -- Healing Touch (40 yards)
-			},
-			resSpells = {
-				[50769] = true, -- Revive (30 yards)
-				[20484] = true, -- Rebirth (30 yards)
-			},
-			petSpells = {},
-		},
-		PALADIN = {
-			enemySpells = {
+				[585] = true, -- Smite (30 yards)
 				[20271] = true, -- Judgement (10 yards)
-			},
-			longEnemySpells = {
-				[879] = true, -- Exorcism (30 yards)
-			},
-			friendlySpells = {
-				[635] = true, -- Holy Light (40 yards)
-			},
-			resSpells = {
-				[7328] = true, -- Redemption (30 yards)
-			},
-			petSpells = {},
-		},
-		SHAMAN = {
-			enemySpells = {
 				[51514] = true, -- Hex (20 yards)
 				[8042] = true, -- Earth Shock (25 yards)
-			},
-			longEnemySpells = {
-				[403] = true, -- Lightning Bolt (30 yards)
-			},
-			friendlySpells = {
-				[331] = true, -- Healing Wave (40 yards)
-			},
-			resSpells = {
-				[2008] = true, -- Ancestral Spirit (30 yards)
-			},
-			petSpells = {},
-		},
-		WARLOCK = {
-			enemySpells = {
 				[5782] = true, -- Fear (20 yards)
-			},
-			longEnemySpells = {
-				[686] = true, -- Shadow Bolt (30 yards)
-			},
-			friendlySpells = {
-				[5697] = true, -- Unending Breath (30 yards)
-			},
-			resSpells = {},
-			petSpells = {
-				[755] = true, -- Health Funnel (45 yards)
-			},
-		},
-		MAGE = {
-			enemySpells = {
-				[2136] = true, -- Fire Blast (20 yards)
-				[12826] = true, -- Polymorph (30 yards)
-			},
-			longEnemySpells = {
-				[133] = true, -- Fireball (35 yards)
-				[44614] = true, -- Frostfire Bolt (40 yards)
-			},
-			friendlySpells = {
-				[475] = true, -- Remove Curse (40 yards)
-			},
-			resSpells = {},
-			petSpells = {},
-		},
-		HUNTER = {
-			enemySpells = {
-				[75] = true, -- Auto Shot (35 yards)
-			},
-			longEnemySpells = {},
-			friendlySpells = {},
-			resSpells = {},
-			petSpells = {
-				[136] = true, -- Mend Pet (45 yards)
-			},
-		},
-		DEATHKNIGHT = {
-			enemySpells = {
 				[49576] = true, -- Death Grip (30 yards)
-			},
-			longEnemySpells = {},
-			friendlySpells = {
-				[47541] = true, -- Death Coil (40 yards)
-			},
-			resSpells = {
-				[61999] = true, -- Raise Ally (30 yards)
-			},
-			petSpells = {},
-		},
-		ROGUE = {
-			enemySpells = {
 				[2094] = true, -- Blind (10 yards)
-			},
-			longEnemySpells = {
-				[26679] = true, -- Deadly Throw (30 yards)
-			},
-			friendlySpells = {
-				[57934] = true, -- Tricks of the Trade (20 yards)
-			},
-			resSpells = {},
-			petSpells = {},
-		},
-		WARRIOR = {
-			enemySpells = {
 				[5246] = true, -- Intimidating Shout (8 yards)
 				[100] = true, -- Charge (25 yards)
 			},
 			longEnemySpells = {
+				[5176] = true, -- Wrath (30 yards)
+				[589] = true, -- Shadow Word: Pain (30 yards)
+				[879] = true, -- Exorcism (30 yards)
+				[403] = true, -- Lightning Bolt (30 yards)
+				[686] = true, -- Shadow Bolt (30 yards)
+				[133] = true, -- Fireball (35 yards)
+				[44614] = true, -- Frostfire Bolt (40 yards)
+				[26679] = true, -- Deadly Throw (30 yards)
 				[355] = true, -- Taunt (30 yards)
 			},
 			friendlySpells = {
+				[5185] = true, -- Healing Touch (40 yards)
+				[2050] = true, -- Lesser Heal (40 yards)
+				[635] = true, -- Holy Light (40 yards)
+				[331] = true, -- Healing Wave (40 yards)
+				[5697] = true, -- Unending Breath (30 yards)
+				[475] = true, -- Remove Curse (40 yards)
+				[47541] = true, -- Death Coil (40 yards)
+				[57934] = true, -- Tricks of the Trade (20 yards)
 				[3411] = true, -- Intervene (25 yards)
 			},
-			resSpells = {},
-			petSpells = {},
-		}
+			resSpells = {
+				[50769] = true, -- Revive (30 yards)
+				[20484] = true, -- Rebirth (30 yards)
+				[2006] = true, -- Resurrection (40 yards)
+				[7328] = true, -- Redemption (30 yards)
+				[2008] = true, -- Ancestral Spirit (30 yards)
+				[61999] = true, -- Raise Ally (30 yards)
+			},
+			petSpells = {
+				[755] = true, -- Health Funnel (45 yards)
+				[136] = true, -- Mend Pet (45 yards)
+			},
+		},
 	}
 }
 
